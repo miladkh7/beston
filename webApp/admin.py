a23c3f524c4522c7b2fa08d6d65d0b33af2faa1e
Milad <milad_khaleghi@live.com>
2019-05-21 02:36:06 +0430

    Add Expence Model

--- webApp/admin.py
+++ webApp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Expence
 # Register your models here.
+admin.site.register(Expence)
+
--- webApp/models.py
+++ webApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Expence(models.Model):
+    text=models.CharField(max_length=20)
+    date=models.DateField()
+    amount=models.BigIntegerField()
+    # user=models.ForeignKey(User)
